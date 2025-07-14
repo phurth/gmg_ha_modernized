a3f6fa96d9cf7280db4f5f464c7a1cc1aa95b95a
phurth <phurth@users.noreply.github.com>
2025-07-14 12:10:59 -0500

    Update __init__.py

--- custom_components/gmg/__init__.py
+++ custom_components/gmg/__init__.py
@@ -1,16 +1,13 @@
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, async_forward_entry_setup, async_forward_entry_unload
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up GMG from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
-    )
+    await async_forward_entry_setup(hass, entry, "climate")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload GMG config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "climate")
-
+    return await async_forward_entry_unload(hass, entry, "climate")
